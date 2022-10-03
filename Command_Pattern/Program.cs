3e9146c54fa2a5c7fb69f32a15439e29d2b0af88
Gowtham <40605124+GowthamF@users.noreply.github.com>
2022-10-03 09:32:44 +0530

    command and strategey

---
+++ Command_Pattern/Program.cs
@@ -0,0 +1,135 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Waiter waiter = new Waiter();
+
+        IMenu juice = new Juice();
+        IMenu meal = new Meal();
+        IMenu dessert = new Dessert();
+
+
+        IOrder juiceOrder = new JuiceOrder(juice);
+        IOrder mealOrder = new MealOrder(meal);
+        IOrder dessertOrder = new DessertOrder(dessert);
+
+        waiter.SetCommand(juiceOrder, mealOrder,dessertOrder);
+
+        waiter.onJuiceOrder();
+        waiter.onMealOrder();
+        waiter.onDessertOrder();
+
+    }
+}
+
+
+public interface IOrder
+{
+    void Execute();
+}
+
+public interface IMenu
+{
+    void PrepareMenu();
+}
+
+public class Juice : IMenu
+{
+    public void PrepareMenu()
+    {
+        Console.WriteLine("Making Juice");
+    }
+}
+
+public class Meal : IMenu
+{
+    public void PrepareMenu()
+    {
+        Console.WriteLine("Preparing Meal");
+    }
+}
+
+public class Dessert : IMenu
+{
+    public void PrepareMenu()
+    {
+        Console.WriteLine("Preparing Dessert");
+    }
+}
+
+public class JuiceOrder : IOrder
+{
+    IMenu _menu;
+
+    public JuiceOrder(IMenu menu)
+    {
+        _menu = menu;
+    }
+
+    public void Execute()
+    {
+        _menu.PrepareMenu();
+    }
+}
+
+public class MealOrder : IOrder
+{
+    IMenu _menu;
+
+
+    public MealOrder(IMenu menu)
+    {
+        _menu = menu;
+    }
+
+    public void Execute()
+    {
+        _menu.PrepareMenu();
+    }
+}
+
+public class DessertOrder : IOrder
+{
+    IMenu _menu;
+
+    public DessertOrder(IMenu menu)
+    {
+        _menu = menu;
+    }
+
+
+    public void Execute()
+    {
+        _menu.PrepareMenu();
+    }
+}
+
+public class Waiter
+{
+    IOrder _juiceOrder;
+    IOrder _mealOrder;
+    IOrder _dessertOrder;
+
+    public void SetCommand(IOrder juiceOrder, IOrder mealOrder, IOrder dessertOrder)
+    {
+        _juiceOrder = juiceOrder;
+        _mealOrder = mealOrder;
+        _dessertOrder = dessertOrder;
+    }
+
+
+    public void onJuiceOrder()
+    {
+        _juiceOrder.Execute();
+    }
+
+    public void onMealOrder()
+    {
+        _mealOrder.Execute();
+    }
+
+    public void onDessertOrder()
+    {
+        _dessertOrder.Execute();
+    }
+}
---
+++ Strategy_Pattern/Program.cs
@@ -0,0 +1,134 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Students poStudents = new PostGraduateStudents();
+        poStudents.OfferPrograms();
+        poStudents.PerformEvent();
+        poStudents.DisplayStudents();
+
+        poStudents.SetFestival(new CodeFest());
+        poStudents.PerformEvent();
+
+    }
+}
+
+
+public interface IFestival
+{
+    void PerformEvent();
+}
+
+public interface IPrograms
+{
+    void OfferPrograms();
+}
+
+public class CodeFest : IFestival
+{
+    public void PerformEvent()
+    {
+        Console.WriteLine("Performing CodeFest");
+    }
+}
+
+public class RoboFest : IFestival
+{
+    public void PerformEvent()
+    {
+        Console.WriteLine("Performing RoboFest");
+    }
+}
+
+public class GameFest : IFestival
+{
+    public void PerformEvent()
+    {
+        Console.WriteLine("Performing GameFest");
+    }
+}
+
+public class DoctoralPrograms : IPrograms
+{
+    public void OfferPrograms()
+    {
+        Console.WriteLine("Offering Doctoral");
+    }
+}
+
+public class MScPrograms : IPrograms
+{
+    public void OfferPrograms()
+    {
+        Console.WriteLine("Offering Msc");
+    }
+}
+
+public class BScPrograms : IPrograms
+{
+    public void OfferPrograms()
+    {
+        Console.WriteLine("Offering Bsc");
+    }
+}
+
+public abstract class Students
+{
+    public IPrograms Programs;
+    public IFestival Festival;
+
+
+    public abstract void DisplayStudents();
+
+
+    public void OfferPrograms()
+    {
+        Programs.OfferPrograms();
+    }
+
+    public void PerformEvent()
+    {
+        Festival.PerformEvent();
+    }
+
+    public void SetProgram(IPrograms program)
+    {
+        Programs = program;
+    }
+
+    public void SetFestival(IFestival festival)
+    {
+        Festival = festival;
+    }
+}
+
+public class UndergraduateStudents : Students
+{
+    public UndergraduateStudents()
+    {
+        Programs = new BScPrograms();
+        Festival = new CodeFest();
+    }
+
+
+    public override void DisplayStudents()
+    {
+        Console.WriteLine("Displaying Undergrads");
+    }
+}
+
+
+public class PostGraduateStudents : Students
+{
+    public PostGraduateStudents()
+    {
+        Programs = new MScPrograms();
+        Festival = new RoboFest();
+    }
+
+
+    public override void DisplayStudents()
+    {
+        Console.WriteLine("Displaying PostGraduates");
+    }
+}
