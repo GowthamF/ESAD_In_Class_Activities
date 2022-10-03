5bba9de32d99f882d409f87bee3284ca4ef437ff
Gowtham <40605124+GowthamF@users.noreply.github.com>
2022-10-03 10:23:47 +0530

    adding week 7 and 8

---
+++ ChainOfResponsibility_Pattern/Program.cs
@@ -0,0 +1,132 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        BestGradeHandler bestGradeHandler = new BestGradeHandler();
+        MeritGradeHandler meritGradeHandler = new MeritGradeHandler();
+        AverageGradeHandler averageGradeHandler = new AverageGradeHandler();
+        FailGradeHandler failGradeHandler = new FailGradeHandler();
+
+        bestGradeHandler.SetSuccessor(meritGradeHandler);
+        meritGradeHandler.SetSuccessor(averageGradeHandler);
+        averageGradeHandler.SetSuccessor(failGradeHandler);
+
+        Console.Write("Enter marks for Grade = ");
+        var mark=Console.ReadLine();
+        
+        var marks = mark != null ? double.Parse(mark) : 0;
+
+        bestGradeHandler.ProcessRequest(new GradeRequest(marks));
+        
+
+    }
+}
+
+
+public abstract class GradeHandler
+{
+    protected GradeHandler? Successor;
+
+    public void SetSuccessor(GradeHandler successor)
+    {
+        Successor = successor;
+    }
+
+    abstract public void ProcessRequest(GradeRequest request);
+}
+
+public class GradeRequest
+{
+    public double Mark
+    {
+        get; set;
+    }
+
+    public GradeRequest(double mark)
+    {
+        Mark = mark;
+    }
+}
+
+public class BestGradeHandler : GradeHandler
+{
+    private const double maxMarks = 100;
+    private const double minMarks = 79;
+
+    public override void ProcessRequest(GradeRequest request)
+    {
+        if (request.Mark > minMarks && request.Mark <= maxMarks)
+        {
+            Console.WriteLine("A");
+        }
+        else
+        {
+            if (Successor != null)
+            {
+                Successor.ProcessRequest(request);
+            }
+        }
+    }
+}
+
+public class MeritGradeHandler : GradeHandler
+{
+    private const double maxMarks = 80;
+    private const double minMarks = 60;
+
+    public override void ProcessRequest(GradeRequest request)
+    {
+        if (request.Mark >= minMarks && request.Mark < maxMarks)
+        {
+            Console.WriteLine("B");
+        }
+        else
+        {
+            if (Successor != null)
+            {
+                Successor.ProcessRequest(request);
+            }
+        }
+    }
+}
+
+public class AverageGradeHandler : GradeHandler
+{
+    private const double maxMarks = 60;
+    private const double minMarks = 39;
+
+    public override void ProcessRequest(GradeRequest request)
+    {
+        if (request.Mark > minMarks && request.Mark <= maxMarks)
+        {
+            Console.WriteLine("C");
+        }
+        else
+        {
+            if (Successor != null)
+            {
+                Successor.ProcessRequest(request);
+            }
+        }
+    }
+}
+
+public class FailGradeHandler : GradeHandler
+{
+    private const double maxMarks = 40;
+
+    public override void ProcessRequest(GradeRequest request)
+    {
+        if (request.Mark < maxMarks)
+        {
+            Console.WriteLine("F");
+        }
+        else
+        {
+            if (Successor != null)
+            {
+                Successor.ProcessRequest(request);
+            }
+        }
+    }
+}
---
+++ Template_Pattern/Program.cs
@@ -0,0 +1,64 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+       Beverages tea = new Tea();
+        tea.PrepareRecepie();
+
+        Beverages coffee = new Coffee();
+        coffee.PrepareRecepie();
+
+    }
+}
+
+
+abstract class Beverages
+{
+    public abstract void Brew();
+    public abstract void AddCondiments();
+
+    public void PrepareRecepie()
+    {
+        BoilWater();
+        Brew();
+        AddCondiments();
+        PourInCup();
+        
+    }
+
+    void BoilWater()
+    {
+        Console.WriteLine("Boiling Water.");
+    }
+
+    void PourInCup()
+    {
+        Console.WriteLine("Pour into cup.");
+    }
+}
+
+class Tea : Beverages
+{
+    public override void AddCondiments()
+    {
+        Console.WriteLine("Adding Lemon");
+    }
+
+    public override void Brew()
+    {
+        Console.WriteLine("Steeping the Tea");
+    }
+}
+
+class Coffee : Beverages
+{
+    public override void AddCondiments()
+    {
+        Console.WriteLine("Adding Sugar and Milk");
+    }
+
+    public override void Brew()
+    {
+        Console.WriteLine("Steeping the coffee through filter");
+    }
+}
